d7eaea2c607de69692e32b44893efc864cb369ba
nidhirastogi <nidhi.gupta@gmail.com>
2019-05-09 16:30:44 -0400

    Updated extractor.py with code that uses sets that store all key words

--- mesh_extract/extractor.py
+++ mesh_extract/extractor.py
@@ -5,11 +5,7 @@ Created on Thu May  9 11:09:25 2019
 
 @author: nidhi rastogi
 """
-
 import csv
-
-    
-
 
 with open('../data_ingestion/MeSH/output/mesh_labels_narrower_broader.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter = '\n')
@@ -18,6 +14,7 @@ with open('../data_ingestion/MeSH/output/mesh_labels_narrower_broader.csv') as c
     narrower = []
     broader = []
     line_count = 0
+    allTokens = set()
     
     for column in csv_reader:
         if line_count == 0:
@@ -31,11 +28,19 @@ with open('../data_ingestion/MeSH/output/mesh_labels_narrower_broader.csv') as c
                     stringcount = len(lst)
                     if stringcount == 4:
                         #print (row)
-                        term.append(lst[0])
-                        label.append(lst[1])
-                        narrower.append(lst[2])
-                        broader.append(lst[3])
+                        term.append(lst[0].strip())
+                        #allTokens.add(lst[0].strip())
+                        label.append(lst[1].strip())
+                        allTokens.add(lst[1].strip())
+                        narrower.append(lst[2].strip())
+                        allTokens.add(lst[2].strip())
+                        broader.append(lst[3].strip())
+                        allTokens.add(lst[3].strip())
                         #print(line_count, f'\t{term} {label} {narrower} {broader}')
             #print(column)
             line_count += 1
+    
+    print(allTokens)
+    
+    #for x in 
     print(f'Processed {line_count} lines.')
