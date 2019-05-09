bbb1422bd954db371f916561c106ec208f0f3348
Oshani Seneviratne <oshanis@gmail.com>
2019-05-08 22:05:08 -0400

    MeSH terms extracted from the latest data dump

--- data_ingestion/MeSH/mesh_load.py
+++ data_ingestion/MeSH/mesh_load.py
@@ -1,5 +1,11 @@
 from rdflib import Graph
 import requests
+import urllib.parse
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+MESH_DOWNLOAD_URL = "ftp://ftp.nlm.nih.gov/online/mesh/rdf/2019/mesh2019.nt.gz"
+BLAZEGRAPH_ENDPOINT_URL = "http://192.168.1.128:9999/blazegraph/sparql?query="
+OUTPUT_FILENAME = 'output/mesh_labels_notes.csv'
 
 def download_file(url):
     """
@@ -16,11 +22,30 @@ def download_file(url):
                     # f.flush()
     return local_filename
 
-# g = Graph()
-# g.parse("data/mesh2019.nt", format="nt")
 
-# print(len(g))
-# import pprint
-# for stmt in g:
-#     pprint.pprint(stmt)
+synonymQuery = """ select distinct ?term ?label ?note 
+{?term <http://id.nlm.nih.gov/mesh/vocab#preferredConcept> ?concept;
+rdfs:label ?label;
+<http://id.nlm.nih.gov/mesh/vocab#note> ?note;
+<http://id.nlm.nih.gov/mesh/vocab#previousIndexing> ?indexing;
+<http://id.nlm.nih.gov/mesh/vocab#source> ?source;
+}"""
 
+termHierarchyQuery = """
+select * 
+{?concept <http://id.nlm.nih.gov/mesh/vocab#narrowerConcept> ?narrowerConcept;
+          rdfs:label ?label.
+ ?narrowerConcept rdfs:label ?narrowerLabel.
+} 
+LIMIT 100
+"""
+
+sparql = SPARQLWrapper("http://192.168.1.128:9999/blazegraph/sparql")
+sparql.setQuery(synonymQuery)
+sparql.setReturnFormat(JSON)
+results = sparql.query().convert()
+
+with open(OUTPUT_FILENAME, 'w') as f:
+    for result in results["results"]["bindings"]:
+        row = str(result["term"]["value"]) + "," + str(result["label"]["value"]) + "," + str(result["note"]["value"])
+        f.write("%s\n" % row)
