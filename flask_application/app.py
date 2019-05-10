d8f157376a7f921ddc03bedb40c3bc1a461d9123
Oshani Seneviratne <oshanis@gmail.com>
2019-05-10 11:07:48 -0400

    Added the dbinit hookups

--- flask_application/app.py
+++ flask_application/app.py
@@ -3,5 +3,7 @@ from pubmed import app
 from pubmed import dbinit
 
 if __name__ == "__main__":
-    dbinit.loadPubMedToMongo()
+    dbinit.dropPubMed()
+    dbinit.loadPubMed()
+    dbinit.queryAll()
     app.run(host='0.0.0.0', port=5000, debug=True)
