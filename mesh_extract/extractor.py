8300c9a007351def8ce8dcf03e317ef54ff5f170
nidhirastogi <nidhi.gupta@gmail.com>
2019-05-10 10:23:06 -0400

    stopstem.py added

--- mesh_extract/extractor.py
+++ mesh_extract/extractor.py
@@ -6,46 +6,64 @@ Created on Thu May  9 11:09:25 2019
 @author: nidhi rastogi
 """
 import csv
+def ingestor():
 
-with open('../data_ingestion/MeSH/output/mesh_labels_narrower_broader.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter = '\n')
-    term = []
-    label = []
-    narrower = []
-    broader = []
-    line_count = 0
-    allTokens = dict()
-    
-    for column in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {"[, .]".join(column)}')
-           
-            line_count += 1
-        else:
-            for row in column:
-                if '|' in row:
-                    lst = row.split("|")
-                    stringcount = len(lst)
-                    if stringcount == 4:
-                        #print (row)
-                        term.append(lst[0].strip())
-                        print(lst[0].strip())
-                        #allTokens.add(lst[0].strip())
-                        label.append(lst[1].strip())
-                        print(lst[1].strip())
-                        allTokens[lst[1].strip()] = lst[0].strip()
-                        narrower.append(lst[2].strip())
-                        print(lst[2].strip())
-                        allTokens[lst[2].strip()] = lst[0].strip()
-                        broader.append(lst[3].strip())
-                        print(lst[3].strip())
-                        allTokens[lst[3].strip()] = lst[0].strip()
-                        #print(line_count, f'\t{term} {label} {narrower} {broader}')
-            #print(column)
-            line_count += 1
-    
-    for key,val in allTokens.items():
-        print (key,':', val)
+    file = '../data_ingestion/MeSH/output/mesh_labels_narrower_broader.csv'
+
+    with open(file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter = '\n')
+        term = []
+        label = []
+        narrower = []
+        broader = []
+        line_count = 0
+        allTokensDict = dict()
+        allTokensList = []
+        
+        for column in csv_reader:
+            if line_count == 0:
+                print(f'Column names are {"[, .]".join(column)}')
+               
+                line_count += 1
+            else:
+                for row in column:
+                    if '|' in row:
+                        lst = row.split("|")
+                        stringcount = len(lst)
+                        if stringcount == 4:
+                            #print (row)
+                            term.append(lst[0].strip())
+                            print(lst[0].strip())
+                            #allTokens.add(lst[0].strip())
+                            
+                            label.append(lst[1].strip())
+                            print(lst[1].strip())
+                            allTokensDict[lst[1].strip()] = lst[0].strip()
+                            allTokensList.append(lst[1].strip())
+                            
+                            narrower.append(lst[2].strip())
+                            print(lst[2].strip())
+                            allTokensDict[lst[2].strip()] = lst[0].strip()
+                            allTokensList.append(lst[2].strip())
+                            
+                            broader.append(lst[3].strip())
+                            print(lst[3].strip())
+                            allTokensDict[lst[3].strip()] = lst[0].strip()
+                            allTokensList.append(lst[3].strip())
+                            #print(line_count, f'\t{term} {label} {narrower} {broader}')
+                #print(column)
+                line_count += 1
+ 
+
+
+def returnListMesh():
+    return allTokensList
+
+def returnDictMesh():
+    return allTokensDict
+        
+        #for key,val in allTokens.items():
+         #   print (key,':', val)
     
     #for x in 
-    print(f'Processed {line_count} lines.')
+    #print(f'Processed {line_count} lines.')
--- mesh_extract/searchQuery.py
+++ mesh_extract/searchQuery.py
@@ -5,12 +5,43 @@ Created on Thu May  9 15:46:22 2019
 
 @author: nidhi rastogi
 """
+#from file import function
 
-import extractor
+from extractor import returnListMesh,returnDictMesh
+from regex import demo
+from stopStem import testFuncOld,testFuncNew,tokenize
+from nltk.tokenize import word_tokenize
+
+
+tokenList = dict()
+meshTermsInQuery = []
+meshDict = dic()
+meshDict = ingestor()
+
+def tokenizeQuery(query):
+     output = [word_tokenize(i) for i in query]
+     return output
 
 def main():
     """ Main program """
-    testString = "Diabetes"
+    # enter search query string 
+    inputString = "Diabetes"
+    
+    #search tokens in mesh terms
+    extractedTokens = tokenizeQuery(inputString)
+    
+    for i in extractedTokens:
+        for j in 
+        demo(i,s2)
+        closest_match_mesh = max()
+        
+        
+        
+        
+    if k in meshDict:
+                print k, my_dict[k]
+        
+    
     
 
     
