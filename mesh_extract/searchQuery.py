65fb24cd043b471a3e329f1d9ab91a05092b3779
nidhirastogi <nidhi.gupta@gmail.com>
2019-05-09 21:18:18 -0400

    Updated extractor.py with code that uses sets that, store all key words. Added regEx.py that uses the Jaro-Wrinkler algorithm for string similarity

---
+++ mesh_extract/searchQuery.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  9 15:46:22 2019
+
+@author: nidhi rastogi
+"""
+
+import extractor
+
+def main():
+    """ Main program """
+    testString = "Diabetes and hyper-tension"
+    
+    
+    
+    
+    
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
